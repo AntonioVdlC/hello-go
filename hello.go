6fc4f6a4d47152f2a363d471f852e2922d3ea302
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-03-31 00:19:30 +0200

    Add stringutil Reverse function

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AntonioVdlC/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, go!")
+	fmt.Println(stringutil.Reverse("Hello, Go!"))
 }
